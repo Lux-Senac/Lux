089755175f879fbf14dfdf3281ec156be0458a19
Cauã <caualeu2016@gmail.com>
2024-03-30 19:16:39 -0300

    Lux v0.0.1

---
+++ src/test/java/br/com/lux/LuxApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.lux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
