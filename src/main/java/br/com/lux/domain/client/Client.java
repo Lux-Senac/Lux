0091e8efbbd1dc9dc96d69b57a4497ac3b698a67
CauaPC <caualeu2016@gmail.com>
2024-04-20 18:26:47 -0300

    Adicionar classe Client ao domínio de clientes

---
+++ src/main/java/br/com/lux/domain/client/Client.java
@@ -0,0 +1,31 @@
+package br.com.lux.domain.client;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity(name = "Clientes")
+@Table(name = "Clientes")
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+public class Client implements Serializable
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private int id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String contato;
+
+    @Column(nullable = false)
+    private String endereco;
+}
