82149c1450357bb94adbb1880358db5729c61316
CauaPC <caualeu2016@gmail.com>
2024-04-21 05:31:04 -0300

    Corrigir controlador de login de usuário e adicionar armazenamento do usuário na sessão

--- src/main/java/br/com/lux/controller/user/login/LoginController.java
+++ src/main/java/br/com/lux/controller/user/login/LoginController.java
@@ -3,6 +3,7 @@ package br.com.lux.controller.user.login;
 import br.com.lux.domain.user.User;
 import br.com.lux.services.user.UserService;
 import br.com.lux.domain.user.UserType;
+import jakarta.servlet.http.HttpSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -29,12 +30,13 @@ public class LoginController
     }
 
     @PostMapping
-    public String loginPost(@RequestParam("email") String email, @RequestParam("password") String password, Model model) {
+    public String loginPost(@RequestParam("email") String email, @RequestParam("password") String password, Model model, HttpSession session) {
         Optional<User> optionalUser = userService.authenticate(email, password);
 
         if(optionalUser.isPresent())
         {
             User user = optionalUser.get();
+            session.setAttribute("user", user); // Armazena o usuário na sessão
 
             if(user.getTipo() == UserType.Admin) {
                 return "redirect:/admin";
