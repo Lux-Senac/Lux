9114fea5fff70906495be0d2771b74aa2df368e2
CauaPC <caualeu2016@gmail.com>
2024-04-21 05:31:09 -0300

    Adicionar interceptor de administração para controle de acesso

---
+++ src/main/java/br/com/lux/Interceptor/AdminInterceptor.java
@@ -0,0 +1,34 @@
+package br.com.lux.Interceptor;
+
+import br.com.lux.domain.user.User;
+import br.com.lux.domain.user.UserType;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.web.servlet.HandlerInterceptor;
+import org.springframework.web.servlet.ModelAndView;
+
+public class AdminInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        User user = (User) request.getSession().getAttribute("user");
+        if (user != null && user.getTipo() == UserType.Admin) {
+            return true;
+        }
+
+        response.sendRedirect("/login");
+        return false;
+    }
+
+    @Override
+    public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler, ModelAndView modelAndView) throws Exception
+    {
+
+    }
+
+    @Override
+    public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex) throws Exception
+    {
+
+    }
+}
