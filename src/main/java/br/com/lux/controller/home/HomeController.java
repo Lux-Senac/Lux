1eef7d732ed53c5ff21809d8401499ec82fd1a37
CauaPC <caualeu2016@gmail.com>
2024-04-20 23:54:03 -0300

    Adicionar método home ao controlador HomeController

--- src/main/java/br/com/lux/controller/home/HomeController.java
+++ src/main/java/br/com/lux/controller/home/HomeController.java
@@ -10,4 +10,9 @@ public class HomeController
     public String index() {
         return "home/index";
     }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home/index";
+    }
 }
