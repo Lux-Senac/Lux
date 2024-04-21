f2795d0c403a0e96175d28055e299cd15672f29e
CauaPC <caualeu2016@gmail.com>
2024-04-21 13:47:10 -0300

    Corrigir estratégia de geração de valor para a chave primária na classe Car

--- src/main/java/br/com/lux/domain/car/Car.java
+++ src/main/java/br/com/lux/domain/car/Car.java
@@ -18,7 +18,7 @@ import java.math.BigDecimal;
 public class Car implements Serializable
 {
     @Id
-    @GeneratedValue(strategy = GenerationType.AUTO)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     @Column(nullable = false)
