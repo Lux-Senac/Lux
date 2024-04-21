e5bf3e13ac0f5b70e5c629fc1e9138babc837ff1
CauaPC <caualeu2016@gmail.com>
2024-04-21 16:05:22 -0300

    Adicionar controlador de logout de usuário

---
+++ src/main/java/br/com/lux/controller/user/login/LogoutController.java
@@ -0,0 +1,20 @@
+package br.com.lux.controller.user.login;
+
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Repository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/logout")
+public class LogoutController extends HttpServlet
+{
+    @GetMapping
+    public String logout(HttpSession session)
+    {
+        session.invalidate();
+        return "redirect:/login";
+    }
+}
