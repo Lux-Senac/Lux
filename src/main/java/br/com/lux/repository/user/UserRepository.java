148fb4882eeea4f9f1c0520af5fe39679bb3a624
CauaPC <caualeu2016@gmail.com>
2024-04-21 01:40:05 -0300

    Corrigir importação e adicionar método findByEmail no UserRepository

--- src/main/java/br/com/lux/repository/user/UserRepository.java
+++ src/main/java/br/com/lux/repository/user/UserRepository.java
@@ -1,9 +1,13 @@
 package br.com.lux.repository.user;
 
+import br.com.lux.domain.user.User;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-@Repository
-public interface UserRepository
-{
+import java.util.Optional;
 
+@Repository
+public interface UserRepository extends JpaRepository<User, Integer>
+{
+    Optional<User> findByEmail(String email);
 }
