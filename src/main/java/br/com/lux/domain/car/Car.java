dea17000351cc73efa868db4c899b351dd5347f9
CauaPC <caualeu2016@gmail.com>
2024-04-20 23:54:40 -0300

    Adicionar importação jakarta.persistence em Car.java

--- src/main/java/br/com/lux/domain/car/Car.java
+++ src/main/java/br/com/lux/domain/car/Car.java
@@ -1,11 +1,11 @@
 package br.com.lux.domain.car;
 
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 
