6fa985a668219157a894cf60f48ce1ff18c464de
CauaPC <caualeu2016@gmail.com>
2024-04-21 01:39:48 -0300

    Corrigir links na página inicial

--- src/main/java/br/com/lux/LuxApplication.java
+++ src/main/java/br/com/lux/LuxApplication.java
@@ -2,6 +2,8 @@ package br.com.lux;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 @SpringBootApplication
 public class LuxApplication {
