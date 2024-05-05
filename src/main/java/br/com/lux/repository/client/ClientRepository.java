97d3c7b7e909ce72925442086cfb4220216e354f
CauaPC <caualeu2016@gmail.com>
2024-05-05 06:43:14 -0300

    Adicionar interface JpaRepository para ClientRepository.java

--- src/main/java/br/com/lux/repository/client/ClientRepository.java
+++ src/main/java/br/com/lux/repository/client/ClientRepository.java
@@ -1,7 +1,11 @@
 package br.com.lux.repository.client;
 
+import br.com.lux.domain.client.Client;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClientRepository {
+public interface ClientRepository extends JpaRepository<Client, Integer>
+{
+
 }
