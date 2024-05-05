30f50be41d79d87822a14cddecad17cc0834c98a
CauaPC <caualeu2016@gmail.com>
2024-05-05 08:11:02 -0300

    Add query to retrieve total sales per car model

--- src/main/java/br/com/lux/repository/sales/SalesRepository.java
+++ src/main/java/br/com/lux/repository/sales/SalesRepository.java
@@ -13,4 +13,10 @@ public interface SalesRepository extends JpaRepository<Sales, Integer> {
     @Query("SELECT c.name AS CarName, s.data_venda AS SaleDate, s.preco_venda AS " +
             "SalePrice FROM Vendas s INNER JOIN s.carro c")
     List<Object[]> findCarSalesDetails();
+
+    @Query("SELECT c.page AS CarModel, COUNT(s) AS TotalSales " +
+            "FROM Vendas s INNER JOIN s.carro c " +
+            "WHERE c.page IN ('TeslaModelS', 'TeslaModelX', 'Porsche', 'Bmw', 'BYDYuan', 'BYDTan') " +
+            "GROUP BY c.page")
+    List<Object[]> findTotalSalesPerCarModel();
 }
