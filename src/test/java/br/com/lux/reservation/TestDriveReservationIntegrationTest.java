569babef5526cf07006f5f2ef1349440f6a6af34
CauaPC <caualeu2016@gmail.com>
2024-05-10 16:32:21 -0300

    refactor: Update pom.xml with new dependencies

--- src/test/java/br/com/lux/reservation/TestDriveReservationIntegrationTest.java
+++ src/test/java/br/com/lux/reservation/TestDriveReservationIntegrationTest.java
@@ -23,7 +23,7 @@ import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+// @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class TestDriveReservationIntegrationTest {
 
     @LocalServerPort
--- src/test/java/br/com/lux/user/UserServiceTest.java
+++ src/test/java/br/com/lux/user/UserServiceTest.java
@@ -11,7 +11,6 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnitRunner;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Optional;
 
