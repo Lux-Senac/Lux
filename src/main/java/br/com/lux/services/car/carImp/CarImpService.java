0f5f8e0844f30ff4ebc1fa51d86f2e2b6622583a
CauaPC <caualeu2016@gmail.com>
2024-05-10 14:26:25 -0300

    refactor: Add validation to CarImpService.registerCar method

--- src/main/java/br/com/lux/services/car/carImp/CarImpService.java
+++ src/main/java/br/com/lux/services/car/carImp/CarImpService.java
@@ -3,18 +3,39 @@ package br.com.lux.services.car.carImp;
 import br.com.lux.domain.car.Car;
 import br.com.lux.repository.car.CarRepository;
 import br.com.lux.services.car.CarService;
+
+import jakarta.validation.ConstraintViolation;
+import jakarta.validation.ConstraintViolationException;
+import jakarta.validation.Validator;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.Set;
 
 @Service
 public class CarImpService implements CarService
 {
     @Autowired
-    private CarRepository carRepository;
+    private final CarRepository carRepository;
+
+    @Autowired
+    private final Validator validator;
+
+    public CarImpService(CarRepository carRepository, Validator validator)
+    {
+        this.carRepository = carRepository;
+        this.validator = validator;
+    }
 
     @Override
     public void registerCar(Car car)
     {
+        Set<ConstraintViolation<Car>> violations = validator.validate(car);
+        if (!violations.isEmpty()) {
+            throw new ConstraintViolationException(violations);
+        }
+
         carRepository.save(car);
     }
 
