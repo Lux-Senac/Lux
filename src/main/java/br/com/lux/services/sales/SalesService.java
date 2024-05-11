086146b86a34189ebd58599c4462625888c39f9e
CauaPC <caualeu2016@gmail.com>
2024-05-11 20:55:53 -0300

    refactor: Update SalesService interface to include methods for finding all sales and registering a sale

--- src/main/java/br/com/lux/services/sales/SalesService.java
+++ src/main/java/br/com/lux/services/sales/SalesService.java
@@ -1,11 +1,17 @@
 package br.com.lux.services.sales;
 
+import br.com.lux.domain.sales.Sales;
+
 import java.util.List;
 import java.util.SequencedCollection;
 
 public interface SalesService
 {
+    List<Sales> findSaleAll();
+
     SequencedCollection<Object[]> findSalesByName(String carNameFilter);
 
     List<Object[]> findTotalSalesPerCarModel();
+
+    void registerSale(Sales sales);
 }
