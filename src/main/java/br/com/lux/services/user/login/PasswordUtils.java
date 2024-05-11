635f2e507ed7d69cad32b8a0cb855d8c6ab7f281
Caua <caualeu2016@gmail.com>
2024-05-10 21:20:27 -0300

    refactor: Add PasswordUtils class for password encryption and validation

---
+++ src/main/java/br/com/lux/services/user/login/PasswordUtils.java
@@ -0,0 +1,13 @@
+package br.com.lux.services.user.login;
+
+import org.mindrot.jbcrypt.BCrypt;
+
+public class PasswordUtils {
+
+    public static String encryptPassword(String password) {
+        return BCrypt.hashpw(password, BCrypt.gensalt());
+    }
+    public static boolean checkPassword(String plainPassword, String encryptedPassword) {
+        return BCrypt.checkpw(plainPassword, encryptedPassword);
+    }
+}
