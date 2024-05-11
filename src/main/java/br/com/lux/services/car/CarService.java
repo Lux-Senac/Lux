3f7a731392982c255df30b5ce735c133da4e6e8b
CauaPC <caualeu2016@gmail.com>
2024-05-11 17:22:10 -0300

    refactor: Add method to retrieve all cars in CarService interface

--- src/main/java/br/com/lux/services/car/CarService.java
+++ src/main/java/br/com/lux/services/car/CarService.java
@@ -2,8 +2,12 @@ package br.com.lux.services.car;
 
 import br.com.lux.domain.car.Car;
 
+import java.util.List;
+
 public interface CarService
 {
+    List<Car> findCarAll();
+
     void registerCar(Car car);
 
     Car findCarById(Integer id);
