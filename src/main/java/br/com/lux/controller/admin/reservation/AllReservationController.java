70c2a61873652435031cdecff2cb6ef4ae850925
CauaPC <caualeu2016@gmail.com>
2024-05-12 13:53:31 -0300

    refactor: Add AllReservationController to handle displaying all reservations in admin panel

---
+++ src/main/java/br/com/lux/controller/admin/reservation/AllReservationController.java
@@ -0,0 +1,30 @@
+package br.com.lux.controller.admin.reservation;
+
+import br.com.lux.domain.user.User;
+import br.com.lux.services.reservation.ReservationService;
+import jakarta.servlet.http.HttpSession;
+
+import org.springframework.ui.Model;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/all-reservation")
+public class AllReservationController
+{
+    @Autowired
+    private ReservationService reservationService;
+
+    @GetMapping
+    public String allReservation(Model model, HttpSession session)
+    {
+        User user = (User) session.getAttribute("user");
+        model.addAttribute("user", user);
+
+        model.addAttribute("reservations", reservationService.findAllReservations());
+
+        return "admin/reservation/gridreservation";
+    }
+}
