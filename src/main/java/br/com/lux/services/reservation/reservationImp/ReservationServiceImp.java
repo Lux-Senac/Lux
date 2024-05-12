9199bbe99789ad6090bcdc69a0373accc2868eed
CauaPC <caualeu2016@gmail.com>
2024-05-12 13:53:47 -0300

    refactor: Add findAllReservations method to ReservationService interface and implement it in ReservationServiceImp class

--- src/main/java/br/com/lux/services/reservation/reservationImp/ReservationServiceImp.java
+++ src/main/java/br/com/lux/services/reservation/reservationImp/ReservationServiceImp.java
@@ -7,11 +7,14 @@ import br.com.lux.domain.reservation.ReservationStatus;
 import br.com.lux.domain.reservation.ReservationType;
 import br.com.lux.repository.reservation.ReservationRepository;
 import br.com.lux.services.reservation.ReservationService;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+
+import java.util.List;
 
 @Service
 public class ReservationServiceImp implements ReservationService
@@ -43,5 +46,11 @@ public class ReservationServiceImp implements ReservationService
         {
             e.printStackTrace();
         }
+    }
+
+    @Override
+    public List<Reservation> findAllReservations()
+    {
+        return reservationRepository.findAll();
     }
 }
