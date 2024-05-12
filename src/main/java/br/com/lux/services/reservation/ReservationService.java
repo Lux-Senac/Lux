86aacdbe88fd0bf5a2d5475f83b0ed975a6fbcdb
CauaPC <caualeu2016@gmail.com>
2024-05-12 13:53:39 -0300

    refactor: Add findAllReservations method to ReservationService interface and implement it in ReservationServiceImp class

--- src/main/java/br/com/lux/services/reservation/ReservationService.java
+++ src/main/java/br/com/lux/services/reservation/ReservationService.java
@@ -3,9 +3,14 @@ package br.com.lux.services.reservation;
 
 import br.com.lux.domain.car.Car;
 import br.com.lux.domain.client.Client;
+import br.com.lux.domain.reservation.Reservation;
 import br.com.lux.domain.reservation.ReservationType;
+
+import java.util.List;
 
 public interface ReservationService
 {
     void registerReservation(Client client, Car car, ReservationType testDrive);
+
+    List<Reservation> findAllReservations();
 }
