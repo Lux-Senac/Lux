0bc84965a7b9fb668df5c3373abe52d9f5bf000c
CauaPC <caualeu2016@gmail.com>
2024-05-11 22:40:01 -0300

    refactor: Update EditSalesController to include sales service and user and client services

--- src/main/java/br/com/lux/controller/admin/sales/EditSalesController.java
+++ src/main/java/br/com/lux/controller/admin/sales/EditSalesController.java
@@ -1,9 +1,16 @@
 package br.com.lux.controller.admin.sales;
 
+import br.com.lux.domain.sales.Sales;
+import br.com.lux.services.car.CarService;
+import br.com.lux.services.client.ClientService;
+import br.com.lux.services.sales.SalesService;
+import br.com.lux.services.user.UserService;
 import jakarta.servlet.http.HttpSession;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -11,9 +18,40 @@ import org.springframework.web.bind.annotation.RequestParam;
 @RequestMapping("/admin/edit-sales")
 public class EditSalesController
 {
+    @Autowired
+    private SalesService salesService;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    private ClientService clientService;
+
     @GetMapping
     public String editSales(@RequestParam("id") Integer id, Model model, HttpSession session)
     {
-        return "admin/sales/editSales";
+        if(id == null)
+        {
+            return "redirect:/admin/all-sales";
+        }
+
+        Sales sales = salesService.findSalesById(id);
+
+        if(sales == null)
+        {
+            return "redirect:/admin/all-sales";
+        }
+
+        model.addAttribute("sales", sales);
+        model.addAttribute("users", userService.findAllUsers());
+        model.addAttribute("clients", clientService.findAllClients());
+
+        return "admin/sales/updatesales";
+    }
+
+    @PostMapping
+    public String editSalesPost()
+    {
+        return "redirect:/admin/all-sales";
     }
 }
