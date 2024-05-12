6390dab03aef9613d0ed53b5deb1dd1b2dcb5a2d
CauaPC <caualeu2016@gmail.com>
2024-05-12 15:14:05 -0300

    refactor: Update RegisterReservationController to use constructor injection for dependencies

--- src/main/java/br/com/lux/controller/admin/reservation/RegisterReservationController.java
+++ src/main/java/br/com/lux/controller/admin/reservation/RegisterReservationController.java
@@ -1,9 +1,19 @@
 package br.com.lux.controller.admin.reservation;
 
+import br.com.lux.domain.reservation.Reservation;
+import br.com.lux.services.car.CarService;
+import br.com.lux.services.client.ClientService;
 import br.com.lux.services.reservation.ReservationService;
+
+import jakarta.servlet.http.HttpSession;
+
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -14,20 +24,45 @@ public class RegisterReservationController
     @Autowired
     private final ReservationService reservationService;
 
-    public RegisterReservationController(ReservationService reservationService)
-    {
+    @Autowired
+    private final CarService carService;
+
+    @Autowired
+    private final ClientService clientService;
+
+    public RegisterReservationController(ReservationService reservationService, CarService carService, ClientService clientService) {
         this.reservationService = reservationService;
+        this.carService = carService;
+        this.clientService = clientService;
     }
 
     @GetMapping
-    public String createReservation()
+    public String createReservation(Model model, HttpSession session)
     {
+        model.addAttribute("user", session.getAttribute("user"));
+        model.addAttribute("reservation", new Reservation());
+        model.addAttribute("cars", carService.findCarAll());
+        model.addAttribute("clients", clientService.findAllClients());
+
         return "admin/reservation/registerrervation";
     }
 
     @PostMapping
-    public String createReservationPost()
+    public String createReservationPost(@Valid @ModelAttribute Reservation reservation, HttpSession session,
+                                        Model model, BindingResult bindingResult)
     {
+        if (bindingResult.hasErrors())
+        {
+            model.addAttribute("user", session.getAttribute("user"));
+            model.addAttribute("cars", carService.findCarAll());
+            model.addAttribute("clients", clientService.findAllClients());
+
+            return "admin/reservation/registerrervation";
+        }
+
+        reservationService.registerReservation(reservation);
+
         return "redirect:/admin/all-reservation";
     }
+
 }
