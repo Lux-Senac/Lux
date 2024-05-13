0d5a90edb7ceb0fd8dac190676e06c697dd28d78
CauaPC <caualeu2016@gmail.com>
2024-05-13 19:39:10 -0300

    refactor: Update EditClientController to handle editing client information in admin panel

--- src/main/java/br/com/lux/controller/admin/client/EditClientCOntroller.java
+++ src/main/java/br/com/lux/controller/admin/client/EditClientCOntroller.java
@@ -1,2 +1,48 @@
-package br.com.lux.controller.admin.client;public class EditClientCOntroller {
+package br.com.lux.controller.admin.client;
+
+import br.com.lux.domain.client.Client;
+import br.com.lux.services.client.ClientService;
+import jakarta.servlet.http.HttpSession;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.*;
+
+@Controller
+@RequestMapping("/admin/edit-client")
+public class EditClientController
+{
+    @Autowired
+    private final ClientService clientService;
+
+    public EditClientController(ClientService clientService) {
+        this.clientService = clientService;
+    }
+
+    @GetMapping
+    public String editClient(@RequestParam("id") Integer id, Model model, HttpSession session)
+    {
+        model.addAttribute("user", session.getAttribute("user"));
+        model.addAttribute("clients", new Client());
+
+        return "admin/client/uptadeclient";
+    }
+
+    @PostMapping
+    public String editClientPost(@Valid @ModelAttribute Client client,
+                                 BindingResult bindingResult, HttpSession session, Model model)
+    {
+        if (bindingResult.hasErrors())
+        {
+            model.addAttribute("user", session.getAttribute("user"));
+
+            return "admin/client/uptadeclient";
+        }
+
+        clientService.registerClient(client);
+
+        return "redirect:/admin/all-clients";
+    }
 }
