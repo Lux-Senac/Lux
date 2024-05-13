dad5a85e06e296cb8aa0636d6df8f5f0f0d04c50
CauaPC <caualeu2016@gmail.com>
2024-05-13 18:54:08 -0300

    refactor: Add AllClientController to handle displaying all clients in admin panel

---
+++ src/main/java/br/com/lux/controller/admin/client/AllClientController.java
@@ -0,0 +1,29 @@
+package br.com.lux.controller.admin.client;
+
+import br.com.lux.services.client.ClientService;
+import jakarta.servlet.http.HttpSession;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/admin/all-clients")
+public class AllClientController
+{
+    private final ClientService clientService;
+
+    public AllClientController(ClientService clientService)
+    {
+        this.clientService = clientService;
+    }
+
+    @GetMapping
+    public String allClient(Model model, HttpSession session)
+    {
+        model.addAttribute("clients", clientService.findAllClients());
+        model.addAttribute("user", session.getAttribute("user"));
+
+        return "admin/client/gridclient";
+    }
+}
