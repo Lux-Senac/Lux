d2413f3c58fb77d16df5ba9f8544b16d0a5b0f38
CauaPC <caualeu2016@gmail.com>
2024-05-19 14:40:16 -0300

    refactor: Add lazy loading for images in admin home page

--- src/main/java/br/com/lux/controller/admin/AdminController.java
+++ src/main/java/br/com/lux/controller/admin/AdminController.java
@@ -1,6 +1,7 @@
 package br.com.lux.controller.admin;
 
-import br.com.lux.domain.user.User;
+import br.com.lux.services.car.CarService;
+import br.com.lux.services.client.ClientService;
 import br.com.lux.services.sales.SalesService;
 
 import jakarta.servlet.http.HttpSession;
@@ -10,25 +11,37 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.List;
-
 @Controller
 @RequestMapping("/admin")
 public class AdminController
 {
-
+    @Autowired
     private final SalesService salesService;
 
     @Autowired
-    public AdminController(SalesService salesService)
+    private final CarService carService;
+
+    @Autowired
+    private final ClientService clientService;
+
+    @Autowired
+    public AdminController(SalesService salesService, CarService carService, ClientService clientService)
     {
         this.salesService = salesService;
+        this.carService = carService;
+        this.clientService = clientService;
     }
 
     @RequestMapping
     public String adminHome(Model model, HttpSession session)
     {
         model.addAttribute("user", session.getAttribute("user"));
+
+        model.addAttribute("totalDeCarros", carService.countCars());
+
+        model.addAttribute("totalDeClientes", clientService.countClients());
+
+        model.addAttribute("totalDeVendasPorMes", salesService.monthlyEarnings());
 
         return "admin/adminHome";
     }
