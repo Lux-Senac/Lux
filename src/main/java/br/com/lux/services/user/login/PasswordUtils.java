b73ce0142acade532fb04e2cbd8f128bb402c0d8
CauaPC <caualeu2016@gmail.com>
2024-05-19 22:48:26 -0300

    refactor: Add method to generate random code in PasswordUtils class

--- src/main/java/br/com/lux/services/user/login/PasswordUtils.java
+++ src/main/java/br/com/lux/services/user/login/PasswordUtils.java
@@ -2,6 +2,8 @@ package br.com.lux.services.user.login;
 
 import org.mindrot.jbcrypt.*;
 
+import java.security.SecureRandom;
+
 public class PasswordUtils {
 
     public static String encryptPassword(String password) {
@@ -9,5 +11,18 @@ public class PasswordUtils {
     }
     public static boolean checkPassword(String plainPassword, String encryptedPassword) {
         return BCrypt.checkpw(plainPassword, encryptedPassword);
+    }
+
+    public static String generateRandomCode() {
+        String characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        SecureRandom random = new SecureRandom();
+        StringBuilder code = new StringBuilder(6);
+
+        for (int i = 0; i < 6; i++) {
+            int randomIndex = random.nextInt(characters.length());
+            code.append(characters.charAt(randomIndex));
+        }
+
+        return code.toString();
     }
 }
