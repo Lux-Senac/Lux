c1c2e95a8598a036fb5638c5755b335f64b89e8e
CauaPC <caualeu2016@gmail.com>
2024-05-19 22:53:24 -0300

    refactor: Add email service to EmailController and inject EmailService via autowiring

--- src/main/java/br/com/lux/controller/email/EmailController.java
+++ src/main/java/br/com/lux/controller/email/EmailController.java
@@ -1,6 +1,7 @@
 package br.com.lux.controller.email;
 
 import br.com.lux.services.email.Email;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,9 +10,10 @@ import org.springframework.web.bind.annotation.RestController;
 import br.com.lux.services.email.EmailService;
 
 @RestController
-@RequestMapping("/admin/email")
+@RequestMapping("/email")
 public class EmailController
 {
+    @Autowired
     private final EmailService emailService;
 
     public EmailController(EmailService emailService)
