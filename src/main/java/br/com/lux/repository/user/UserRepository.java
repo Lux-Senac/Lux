fb0c8baa010ad61f59c82ff3f796c3cb1410ebf2
CauaPC <caualeu2016@gmail.com>
2024-05-20 20:28:01 -0300

    refactor: Update UserRepository with transactional annotations for findByEmail and findByUsername

--- src/main/java/br/com/lux/repository/user/UserRepository.java
+++ src/main/java/br/com/lux/repository/user/UserRepository.java
@@ -1,13 +1,20 @@
 package br.com.lux.repository.user;
 
 import br.com.lux.domain.user.User;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
 
 @Repository
 public interface UserRepository extends JpaRepository<User, Integer>
 {
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     Optional<User> findByEmail(String email);
+
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    Optional<User> findByUsername(String username);
 }
