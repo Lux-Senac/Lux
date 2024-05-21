cad60ffd0458dd59c9d6a364cea49117afb49bbf
CauaPC <caualeu2016@gmail.com>
2024-05-20 21:06:03 -0300

    chore: Add @Transactional annotation to ClientRepository's findByUsersIsNull method

--- src/main/java/br/com/lux/repository/client/ClientRepository.java
+++ src/main/java/br/com/lux/repository/client/ClientRepository.java
@@ -3,11 +3,14 @@ package br.com.lux.repository.client;
 import br.com.lux.domain.client.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client, Integer>
 {
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     List<Client> findByUsersIsNull();
 }
