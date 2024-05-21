1422e7608c51c97493b6c2599f71819d350f1ad4
CauaPC <caualeu2016@gmail.com>
2024-05-20 23:31:03 -0300

    refactor: Add @Transactional(propagation = Propagation.NOT_SUPPORTED) to repository methods

--- src/main/java/br/com/lux/repository/reservation/ReservationRepository.java
+++ src/main/java/br/com/lux/repository/reservation/ReservationRepository.java
@@ -4,9 +4,12 @@ import br.com.lux.domain.reservation.Reservation;
 import br.com.lux.domain.reservation.ReservationStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @Repository
 public interface ReservationRepository extends JpaRepository<Reservation, Integer>
 {
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     long countByStatusreserva(ReservationStatus statusreserva);
 }
--- src/main/java/br/com/lux/repository/sales/SalesRepository.java
+++ src/main/java/br/com/lux/repository/sales/SalesRepository.java
@@ -4,6 +4,8 @@ import br.com.lux.domain.sales.Sales;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.math.BigDecimal;
 import java.util.Date;
@@ -12,18 +14,22 @@ import java.util.List;
 @Repository
 public interface SalesRepository extends JpaRepository<Sales, Integer> {
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     @Query("SELECT c.name AS CarName, s.datavenda AS SaleDate, s.precovenda AS " +
             "SalePrice FROM Vendas s INNER JOIN s.carro c")
     List<Object[]> findCarSalesDetails();
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     @Query("SELECT c.page AS CarModel, COUNT(s) AS TotalSales " +
             "FROM Vendas s INNER JOIN s.carro c " +
             "WHERE c.page IN ('TeslaModelS', 'TeslaModelX', 'Porsche', 'Bmw', 'BYDYuan', 'BYDTan') " +
             "GROUP BY c.page")
     List<Object[]> findTotalSalesPerCarModel();
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     @Query("SELECT SUM(s.precovenda) FROM Vendas s WHERE FUNCTION('MONTH', s.datavenda) = FUNCTION('MONTH', :data) AND FUNCTION('YEAR', s.datavenda) = FUNCTION('YEAR', :data)")
     BigDecimal ganhosMensais(Date data);
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
     List<Sales> findByDatavendaBetween(Date dataInicio, Date dataFim);
 }
