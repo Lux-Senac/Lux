2729bc196b5904172b29a4ce9b0c1853689f22d9
CauaPC <caualeu2016@gmail.com>
2024-05-23 17:59:53 -0300

    feat: Update Car table with new models and specifications

--- src/main/resources/db/data.sql
+++ src/main/resources/db/data.sql
@@ -12,19 +12,16 @@ INSERT INTO Clientes (nome, sobrenome, contato, email, cep, pais, preferenciacon
 ('Nome10', 'Sobrenome10', '1234567899', 'email10@email.com', '12354', 'BRASIL', 'WHATSAPP');
 
 -- Inserts for Car table
-INSERT INTO Car (name, motor, cil, acel, hp, torque, velmax, doors, seats, val, price, image, title, page)
-VALUES ('Nome do Carro', 'Motor do Carro', 'Cilindro do Carro', 'Aceleração do Carro', 'Potência do Carro', 'Torque do Carro', 'Velocidade Máxima do Carro', 4, 5, 'Valor da Válvula do Carro', 50000.00, 'https://i.pinimg.com/originals/76/ea/9b/76ea9b37388c46c67b718a5ec6b4ad33.gif', 'Descrição do Carro', 'Bmw');
 INSERT INTO Car (name, motor, cil, acel, hp, torque, velmax, doors, seats, val, price, image, title, page) VALUES
-('Tesla Model S', 'Motor1', 'Cil1', 'Acel1', 'HP1', 'Torque1', 'VelMax1', 4, 5, 'Val1', 80000.00, 'https://i.pinimg.com/originals/1a/4c/5b/1a4c5bb4855f4d06caa78471692c5f9a.gif', 'Title1', 'TeslaModelS'),
-('Tesla Model X', 'Motor2', 'Cil2', 'Acel2', 'HP2', 'Torque2', 'VelMax2', 4, 5, 'Val2', 85000.00, 'https://i.pinimg.com/originals/4f/12/e3/4f12e32fe10f4aeb22003a950c43e166.gif', 'Title2', 'TeslaModelX'),
-('Porsche Taycan Turbo S', 'Motor3', 'Cil3', 'Acel3', 'HP3', 'Torque3', 'VelMax3', 2, 2, 'Val3', 150000.00, 'https://i.pinimg.com/originals/72/61/dc/7261dc7c0a0923d7fbcbe288ed5ff403.gif', 'Title3', 'Porsche'),
-('Bmw I8', 'Motor4', 'Cil4', 'Acel4', 'HP4', 'Torque4', 'VelMax4', 2, 2, 'Val4', 140000.00, 'https://i.pinimg.com/originals/0a/d4/65/0ad465088cee13d5dab25c06955223b9.gif', 'Title4', 'Bmw'),
-('BYD Yuan', 'Motor5', 'Cil5', 'Acel5', 'HP5', 'Torque5', 'VelMax5', 4, 5, 'Val5', 35000.00, 'https://i.pinimg.com/originals/e1/80/36/e18036de3669defc2f0f6a05533d5e6b.gif', 'Title5', 'BYDYuan'),
-('BYD Tan', 'Motor6', 'Cil6', 'Acel6', 'HP6', 'Torque6', 'VelMax6', 4, 5, 'Val6', 40000.00, 'https://i.pinimg.com/originals/78/0a/ff/780aff211e0e33d9349c35f614a2ea44.gif', 'Title6', 'BYDTan');
-
+('Tesla Model S', 'Dois Motores Elétricos','651 Km', '3,1s', '605 cv', '92,48 Kgfm', '250 Km/h', 4, 5, 'AWD', 5.299, 'https://i.pinimg.com/originals/e9/eb/b5/e9ebb543389a2e5122e6d883eac3bc6b.jpg', 'Comforto e dirigibilidade com tecnologia.', 'TeslaModelS'),
+('Tesla Model X', 'Dois Motores Elétricos', '400 Km', '4s', '772 cv', '94,8 Kgfm', '250 Km/h', 4, 7, 'AWD', 7.350, 'https://images.alphacoders.com/121/thumb-1920-1210651.jpg', 'Seguranca e Potencia em um carro.', 'TeslaModelX'),
+('Porsche Taycan Turbo S', 'Dois Motores Elétricos', '388 Km', '2,8s', '761 cv', '107,1 Kgfm', '260 Km/h', 4, 4, 'AWD', 12.000, 'https://images6.alphacoders.com/104/thumb-1920-1041796.jpg', 'Um toque de luxuosidade e desempenho', 'Porsche'),
+('Bmw I8', 'Intercooled Turbo Gas/Electric I-3','600 km', '4,4s', '369 cv', '42 kgfm', '250 Km/h', 2, 4, 'AWD', 7.899, 'https://images7.alphacoders.com/549/thumb-1920-549793.jpg', 'Um toque de luxuosidade e desempenho', 'Bmw'),
+('BYD Yuan', 'Elétrico', '294 Km', '7,3s', '204 cv', '31,0 Kgfm', '160 Km/h', 4, 5, 'FWD', 3.599, 'https://i.pinimg.com/736x/60/99/fb/6099fbf9e2b828de6105eec182034dab.jpg', 'Carro família e econômico', 'BYDYuan'),
+('BYD Tan', 'Elétrico', '306 Km', '4,6s', '272 cv', '69,3 Kgfm', '186 Km/h', 4, 7, 'AWD', 6.200, 'https://www.cnnbrasil.com.br/wp-content/uploads/sites/12/2024/04/byd-tan-2024.webp', 'O SUV elétrico para a família toda', 'BYDTan');
 
 -- Inserts for USR table
-INSERT INTO USR (username, password, email, tipo, urlavatar) VALUES ('User 1', '$2a$10$82J2fDP6kak99vcroDzAFOmxrjHQxVo/LhhVmcbPycbWWRW7Y43K2', 'user1@example.com', 'ADMIN', 'https://www.gravatar.com/avatar/0f8ededc78c2d265f41bb2ef64cbb6f1ea551bc15b2b72ee552f5fc2084e9671');
+INSERT INTO USR (username, password, email, tipo, urlavatar) VALUES ('User 1', '$2a$10$82J2fDP6kak99vcroDzAFOmxrjHQxVo/LhhVmcbPycbWWRW7Y43K2', 'user1@example.com', 'ADMIN', 'https://i.pinimg.com/originals/72/61/dc/7261dc7c0a0923d7fbcbe288ed5ff403.gif');
 INSERT INTO USR (username, password, email, tipo, id_cliente, urlavatar) VALUES ('User 2', '$2a$10$z0p6uXfjFrmkEv5O4aMdAOY1PFJcZNzQjmcMMux.QPV/AemxwUdkC', 'user2@example.com', 'CLIENTE', 1,'https://www.gravatar.com/avatar/0f8ededc78c2d265f41bb2ef64cbb6f1ea551bc15b2b72ee552f5fc2084e9671');
 
 -- Inserts for Vendas table
