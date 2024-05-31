ff87a8bdcc30c7ccd640ac3c5efa0dfa0a40454f
CauaPC <caualeu2016@gmail.com>
2024-05-31 04:57:21 -0300

    feat: Adicionar funcionalidade de paginação e busca na tabela de dados

--- src/main/java/br/com/lux/controller/admin/user/AllUserController.java
+++ src/main/java/br/com/lux/controller/admin/user/AllUserController.java
@@ -1,15 +1,22 @@
 package br.com.lux.controller.admin.user;
 
-import br.com.lux.services.exception.ServiceException;
+import br.com.lux.domain.user.User;
 import br.com.lux.services.user.UserService;
 
 import jakarta.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/admin/all-users")
@@ -23,26 +30,32 @@ public class AllUserController
         this.userService = userService;
     }
 
+    private final Map<String, Object> response = new HashMap<>();
+
     @GetMapping
     public String allUsers(Model model, HttpSession session)
     {
-        try
-        {
-            model.addAttribute("users", userService.findAllUsers());
-
             return "admin/user/griduser";
-        }
-        catch (ServiceException e)
-        {
-            model.addAttribute("error", e.getMessage());
+    }
 
-            return "admin/user/griduser";
-        }
-        catch (Exception e)
-        {
-            model.addAttribute("error", "Erro ao buscar usuários.");
+    @GetMapping("/json")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> allUsersJson(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            @RequestParam(value = "search[value]", required = false) String searchTerm)
+    {
+        Page<User> users;
 
-            return "admin/user/griduser";
+        if (searchTerm != null && !searchTerm.isEmpty()) {users = userService.searchUsers(searchTerm, page, size);
+        } else {
+            users = userService.findAllUsers(page, size);
         }
+
+        response.put("data", users.getContent());
+        response.put("iTotalRecords", users.getTotalElements());
+        response.put("iTotalDisplayRecords", users.getTotalElements());
+
+        return ResponseEntity.ok(response);
     }
 }
