96cfd6c691c0971e0e0e5547b916b86c7107e7af
CauaPC <caualeu2016@gmail.com>
2024-05-31 01:53:26 -0300

    feat: Add pagination and search functionality to CarController
    The recent code changes in `CarController.java` include the addition of pagination and search functionality. This allows users to retrieve all cars with pagination and search for specific cars based on certain criteria. The changes were made to improve the user experience and make it easier to find and navigate through the car listings.

--- src/main/java/br/com/lux/controller/car/CarController.java
+++ src/main/java/br/com/lux/controller/car/CarController.java
@@ -3,8 +3,10 @@ package br.com.lux.controller.car;
 import br.com.lux.domain.car.Car;
 import br.com.lux.repository.car.CarRepository;
 
+import br.com.lux.services.car.CarService;
 import br.com.lux.services.exception.ServiceException;
 import org.jetbrains.annotations.NotNull;
+import org.springframework.data.domain.Page;
 import org.springframework.stereotype.Component;
 import org.springframework.ui.Model;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,44 +24,41 @@ import java.util.List;
 public class CarController
 {
     @Autowired
-    private final CarRepository carRepository;
+    private final CarService carService;
 
     @Autowired
-    public CarController(CarRepository carRepository)
+    public CarController(CarService carService)
     {
-        this.carRepository = carRepository;
+        this.carService = carService;
     }
 
     @GetMapping("/find-all")
-    public String findAllCars(Model model, RedirectAttributes redirectAttributes)
+    public String findAllCars(
+            @RequestParam(defaultValue = "0") int page,
+            @RequestParam(defaultValue = "10") int size,
+            Model model, RedirectAttributes redirectAttributes
+    )
     {
         try
         {
-            List<Car> cars = carRepository.findAll();
+            Page<Car> cars = carService.findCarAll(page, size);
             model.addAttribute("cars", cars);
-
+            model.addAttribute("currentPage", page);
             return "offers/ofertas";
         }
         catch (ServiceException e)
         {
-            redirectAttributes.addFlashAttribute("error", "Erro ao buscar carros");
-
-            return "redirect:/home";
-        }
-        catch (Exception e)
-        {
-            redirectAttributes.addFlashAttribute("error", "Erro");
-
+            redirectAttributes.addFlashAttribute("error", e.getMessage());
             return "redirect:/home";
         }
     }
 
     @GetMapping("/detalhes-carro")
-    public String findById(@RequestParam("id") Integer id, Model model, RedirectAttributes redirectAttributes)
+    public String findById(@RequestParam("id") Integer id, Model model)
     {
         try
         {
-            Car car = carRepository.findById(id).orElseThrow(() -> new IllegalArgumentException("Carro não encontrado"));
+            Car car = carService.findCarById(id);
 
             model.addAttribute("car", car);
             return "cars/" + car.getPage();
@@ -67,12 +66,6 @@ public class CarController
         catch (ServiceException e)
         {
             model.addAttribute("error", e.getMessage());
-
-            return "redirect:/carros/find-all";
-        }
-        catch (Exception e)
-        {
-            model.addAttribute("error", "Erro");
 
             return "redirect:/carros/find-all";
         }
