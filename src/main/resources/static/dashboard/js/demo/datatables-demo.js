c355207ce714600683627f7f7b1ae9c9f6fb7ad9
CauaPC <caualeu2016@gmail.com>
2024-05-31 01:56:29 -0300

    feat: Refatorar código de cores na tabela de dados
    Esta alteração refatora o código de cores na tabela de dados para melhorar a legibilidade e manutenibilidade do código. Os estilos de cores foram movidos para classes CSS separadas, tornando mais fácil adicionar ou modificar cores no futuro.

--- src/main/resources/static/dashboard/js/demo/datatables-demo.js
+++ src/main/resources/static/dashboard/js/demo/datatables-demo.js
@@ -19,39 +19,7 @@ $(document).ready(function() {
       } else if (reservationStatus === 'REJEITADO') {
         $('td:eq(5)', row).css('color', 'red');
       }
-
-      // Obter os valores de Preferência de contado
-      var contactPreference = data[3]; // Índice da coluna de preferência de contato
-      var pais = data[4]; // Índice da coluna de país
-
-      // Aplicar cores com base nos valores
-      if(contactPreference === 'EMAIL') {
-        $('td:eq(3)', row).css('color', 'blue');
-      } else if(contactPreference === 'TELEFONE') {
-        $('td:eq(3)', row).css('color', 'red');
-      }else if(contactPreference === 'WHATSAPP') {
-        $('td:eq(3)', row).css('color', 'green');
-      }
-
-      // Aplicar cores com base nos valores
-      if(pais === 'BRASIL') {
-        $('td:eq(4)', row).css('color', 'green');
-      } else if(pais === 'ARGENTINA') {
-        $('td:eq(4)', row).css('color', 'blue');
-      } else if(pais === 'CHILE') {
-        $('td:eq(4)', row).css('color', 'red');
-      } else if(pais === 'URUGUAI') {
-        $('td:eq(4)', row).css('color', 'yellow');
-      } else if(pais === 'PARAGUAI') {
-        $('td:eq(4)', row).css('color', 'orange');
-      } else if(pais === 'PERU') {
-        $('td:eq(4)', row).css('color', 'purple');
-      } else if(pais === 'COLÔMBIA') {
-        $('td:eq(4)', row).css('color', 'pink');
-      } else if(pais === 'VENEZUELA') {
-        $('td:eq(4)', row).css('color', 'brown');
-      }
-
     }
   });
 });
+
