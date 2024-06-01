3d28904de23ab6ad1bbab054c271275bdc491505
CauaPC <caualeu2016@gmail.com>
2024-06-01 19:35:05 -0300

    feat: Adicionar método para buscar todos os usuários por tipo

--- src/main/java/br/com/lux/repository/user/UserRepository.java
+++ src/main/java/br/com/lux/repository/user/UserRepository.java
@@ -2,6 +2,7 @@ package br.com.lux.repository.user;
 
 import br.com.lux.domain.user.User;
 
+import br.com.lux.domain.user.UserType;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Repository;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
@@ -24,4 +26,6 @@ public interface UserRepository extends JpaRepository<User, Integer>
     @Query("SELECT u FROM USR u WHERE LOWER(u.username) LIKE LOWER(CONCAT('%', :term, '%')) OR LOWER(u.email) LIKE LOWER(CONCAT('%', :term, '%'))")
     Page<User> findByUsernameOrEmailContaining(String term, Pageable pageable);
 
+    @Transactional(propagation = Propagation.NOT_SUPPORTED)
+    List<User> findByTipo(UserType tipo);
 }
